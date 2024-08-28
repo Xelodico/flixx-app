55fe6991f10ffecd4e29cebe5372b10b6d11f8ef
Peter Robinson <peter0898mail5@gmail.com>
2024-08-28 16:01:29 +0100

    Feat: Display popular movies

--- js/script.js
+++ js/script.js
@@ -4,6 +4,54 @@ const global = {
 
 console.log(global.currentPage);
 
+async function displayPopularMovies() {
+  const { results } = await fetchAPIData("movie/popular");
+
+  console.log(results);
+
+  results.forEach((movie) => {
+    const div = document.createElement("div");
+    div.classList.add("card");
+
+    div.innerHTML = `
+          <a href="movie-details.html?id=${movie.id}">
+${
+  movie.poster_path
+    ? `<img
+              src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
+              class="card-img-top"
+              alt="${movie.title}"
+            />`
+    : `<img
+            src="images/no-image.jpg"
+            class="card-img-top"
+            alt="${movie.title}"
+          />`
+}
+          </a>
+          <div class="card-body">
+            <h5 class="card-title">${movie.title}</h5>
+            <p class="card-text">
+              <small class="text-muted">Release: ${movie.release_date}</small>
+            </p>
+          </div>`;
+    document.querySelector("#popular-movies").appendChild(div);
+  });
+}
+
+// Fetch data from TMDB API
+async function fetchAPIData(endpoint) {
+  const API_KEY = "bc757194559843f0ed4c5af750c0e8ed";
+  const API_URL = "https://api.themoviedb.org/3/";
+
+  const response = await fetch(
+    `${API_URL}${endpoint}?api_key=${API_KEY}&language=en-GB`
+  );
+
+  const data = await response.json();
+  return data;
+}
+
 // Highlight active link
 function highlightActiveLink() {
   const navLinks = document.querySelectorAll(".nav-link");
@@ -15,12 +63,12 @@ function highlightActiveLink() {
   });
 }
 
-// Init App
+// Init App, page router
 function init() {
   switch (global.currentPage) {
     case "/":
     case "/index.html":
-      console.log("Home");
+      displayPopularMovies();
       break;
     case "/shows.html":
       console.log("Shows");
