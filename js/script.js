a1b560359199de395c7c3b7fb3819be4446218ec
Peter Robinson <peter0898mail5@gmail.com>
2024-08-29 15:08:42 +0100

    Feat: TV show details page

--- js/script.js
+++ js/script.js
@@ -145,6 +145,75 @@ async function displayMovieDetails() {
   document.querySelector("#movie-details").appendChild(div);
 }
 
+// Display Show Details
+async function displayShowDetails() {
+  const showID = window.location.search.split("=")[1];
+
+  const show = await fetchAPIData(`tv/${showID}`);
+
+  // Overlay for background image
+  displayBackgroundImage("tv", show.backdrop_path);
+
+  const div = document.createElement("div");
+
+  div.innerHTML = `<div class="details-top">
+  <div>
+  ${
+    show.poster_path
+      ? `<img
+      src="https://image.tmdb.org/t/p/w500${show.poster_path}"
+      class="card-img-top"
+      alt="${show.name}"
+    />`
+      : `<img
+      src="images/no-image.jpg"
+      class="card-img-top"
+      alt="${show.name}"
+    />`
+  }
+
+  </div>
+  <div>
+    <h2>${show.name}</h2>
+    <p>
+      <i class="fas fa-star text-primary"></i>
+      ${show.vote_average.toFixed(1)} / 10
+    </p>
+    <p class="text-muted">Last Air Date: ${show.last_air_date}</p>
+    <p>
+      ${show.overview}
+    </p>
+    <h5>Genres</h5>
+    <ul class="list-group">
+    ${show.genres.map((genre) => `<li>${genre.name}</li>`).join("")}
+    </ul>
+    <a href="${
+      show.homepage
+    }" target="_blank" class="btn">Visit Show Homepage</a>
+  </div>
+</div>
+<div class="details-bottom">
+  <h2>Show Info</h2>
+  <ul>
+    <li><span class="text-secondary">Number Of Episodes:</span> ${
+      show.number_of_episodes
+    }</li>
+    <li>
+      <span class="text-secondary">Last Episode To Air:</span> ${
+        show.last_episode_to_air.name
+      }
+    </li>
+    <li><span class="text-secondary">Status:</span> ${show.status}</li>
+  </ul>
+  <h4>Production Companies</h4>
+  <div class="list-group">${show.production_companies
+    .map((company) => `<span>${company.name}</span>`)
+    .join(", ")}</div>
+</div>`;
+
+  document.querySelector("#show-details").appendChild(div);
+}
+
 // Display Backdrop on Details pages
 function displayBackgroundImage(type, backgroundPath) {
   const overlayDiv = document.createElement("div");
@@ -158,7 +227,7 @@ function displayBackgroundImage(type, backgroundPath) {
   overlayDiv.style.top = "0";
   overlayDiv.style.left = "0";
   overlayDiv.style.zIndex = "-1";
-  overlayDiv.style.opacity = "0.15";
+  overlayDiv.style.opacity = "0.1";
 
   if (type === "movie") {
     document.querySelector("#movie-details").appendChild(overlayDiv);
@@ -221,7 +290,7 @@ function init() {
       displayMovieDetails();
       break;
     case "/tv-details.html":
-      console.log("TV Details");
+      displayShowDetails();
       break;
     case "/search.html":
       console.log("Search");
